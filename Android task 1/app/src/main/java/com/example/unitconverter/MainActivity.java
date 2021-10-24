40da305376cdadce8e87b4a45cfaa398f46ee78e
anas012 <anas@salesassist.pk>
2021-10-24 23:11:53 +0500

    Image added

--- Android task 1/app/src/main/java/com/example/unitconverter/MainActivity.java
+++ Android task 1/app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.unitconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void btnclick(View View)
+    public void btnclick(View view)
     {
         EditText gold=findViewById(R.id.editText1);
         EditText silver=findViewById(R.id.editText2);
@@ -26,12 +27,24 @@ public class MainActivity extends AppCompatActivity {
         EditText otheramt=findViewById(R.id.editText4);
         String strgold=gold.getText().toString();
         Double doublegold=Double.parseDouble(strgold);
-        String strsilver=gold.getText().toString();
+        String strsilver=silver.getText().toString();
         Double doublesilver=Double.parseDouble(strsilver);
         String strcash=cash.getText().toString();
         Double doublecash=Double.parseDouble(strcash);
         String strotheramt=otheramt.getText().toString();
         Double doubleotheramt=Double.parseDouble(strotheramt);
+
+        Double sum=doublesilver+doublegold+doublecash+doubleotheramt;
+
+        if(gold.equals("")&&silver.equals("")&&cash.equals("")&&otheramt.equals(""))
+        {
+            Toast.makeText(this,"Please enter the values",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Double zakat = sum * 0.025;
+            String toasttext = "Your amount of zakat is " + zakat.toString() +" "+ "Rs";
+            Toast.makeText(this, toasttext, Toast.LENGTH_SHORT).show();
+        }
 
 
     }
