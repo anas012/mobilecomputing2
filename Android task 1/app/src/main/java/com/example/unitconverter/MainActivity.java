246fc0eb5378eb9ef8d13e941f18ea89285747ae
anas012 <anas@salesassist.pk>
2021-10-24 21:22:44 +0500

    Layout created

--- Android task 1/app/src/main/java/com/example/unitconverter/MainActivity.java
+++ Android task 1/app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.unitconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner myspiner1 = findViewById(R.id.spineer1);
-        ArrayAdapter<String>myadpter=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.unitnames1));
-    myadpter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    myspiner1.setAdapter(myadpter);
 
 
-        Spinner myspiner2 = findViewById(R.id.spineer2);
-        ArrayAdapter<String>myadpter2=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.unitnames2));
-        myadpter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        myspiner2.setAdapter(myadpter2);
+    }
+    public void btnclick(View View)
+    {
+//        EditText value=findViewById(R.id.text1);
+//        String value1=value.getText().toString();
+//        Double doublevalue=Double.parseDouble(value1);
+
     }
 }
